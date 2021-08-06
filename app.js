f70a65e5abda654b359b933778a63ea2161571b8
Suryansh Chopra <suryanshchopra987@gmail.com>
2021-08-07 00:41:11 +0530

    Adding API to get current stock market price

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 form = document.forms[0];
-inputs = document.querySelectorAll(".input");
+input1 = document.querySelector("#input1");
+input2 = document.querySelector("#input2");
+input3 = document.querySelector("#input3");
+
 output = document.querySelector(".output");
 contentDiv = document.querySelector(".displayFlex");
 cols = document.querySelectorAll(".col");
 
-     
+
+// var serverURL = "https://api.tiingo.com/tiingo/daily/"
+
+// function getTranslationURL(text){
+//     return serverURL + text +"/prices"
+// }
+
 
 form.addEventListener("submit", checkHandler);
 
@@ -14,10 +23,37 @@ function checkHandler(e){
     // cols[0].classList.remove("transparentBg");
     // cols[1].classList.remove("transparentBg");
 
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Token d6f8b32a099dd663ee3a1f418285a3928a77399b");
+    myHeaders.append("Cookie", "sessionid=5xfxsp6puxg09u3er56b4o5t5mlzdhxs");
+    myHeaders.append("User-Agent", "Mozilla/5.0");
+    myHeaders.append("Access-Control-Allow-Origin", "null");
 
-    let CP = inputs[0].value;
-    let Qty = inputs[1].value;
-    let SP = inputs[2].value;
+
+
+    var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+    };
+
+    // fetch(getTranslationURL(CP), requestOptions)
+    // .then(response => response.text())
+    // .then(result => console.log(result))
+    // .catch(error => console.log('error', error));
+
+
+    fetch("https://api.tiingo.com/tiingo/daily/AAPL/prices", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+    
+
+    let CP = input1.value;
+    let Qty = input2.value;
+    let SP = input3.value;
 
     if( !isNaN(CP) && !isNaN(Qty) && !isNaN(SP)) {
         CP = Number(CP);
