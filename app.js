90d0fbb296ae8914b3a40eba1268a71486aea9f7
Suryansh Chopra <suryanshchopra987@gmail.com>
2021-08-11 21:42:59 +0530

    Working model updated

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 form = document.forms[0];
+input0 = document.querySelector("#input0");
 input1 = document.querySelector("#input1");
 input2 = document.querySelector("#input2");
 input3 = document.querySelector("#input3");
 
 output = document.querySelector(".output");
+output0 = document.querySelector(".output0");
 contentDiv = document.querySelector(".displayFlex");
 cols = document.querySelectorAll(".col");
 
 
-// var serverURL = "https://api.tiingo.com/tiingo/daily/"
 
-// function getTranslationURL(text){
-//     return serverURL + text +"/prices"
-// }
+var serverURL = "https://cors-anywhere.herokuapp.com/https://api.tiingo.com/tiingo/daily/"
 
+function getTranslationURL(text){
+    return serverURL + text +"/prices"
+    
+}
+
+var SP = 0;
 
 form.addEventListener("submit", checkHandler);
 
 
 function checkHandler(e){
     e.preventDefault();
-    // cols[0].classList.remove("transparentBg");
-    // cols[1].classList.remove("transparentBg");
-
+    
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Authorization", "Token d6f8b32a099dd663ee3a1f418285a3928a77399b");
     myHeaders.append("Cookie", "sessionid=5xfxsp6puxg09u3er56b4o5t5mlzdhxs");
     myHeaders.append("User-Agent", "Mozilla/5.0");
-    myHeaders.append("Access-Control-Allow-Origin", "null");
-
+   
+    let inputText = input0.value;
 
 
     var requestOptions = {
@@ -38,40 +41,38 @@ function checkHandler(e){
     redirect: 'follow'
     };
 
-    // fetch(getTranslationURL(CP), requestOptions)
-    // .then(response => response.text())
-    // .then(result => console.log(result))
-    // .catch(error => console.log('error', error));
-
-
-    fetch("https://api.tiingo.com/tiingo/daily/AAPL/prices", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-
-    
+    fetch(getTranslationURL(inputText), requestOptions)
+    .then(response => response.json())
+    // .then(json => console.log(json[0].adjClose))
+    .then(json => {
+        
+        var translatedText = json[0].adjClose;
+        SP = translatedText;
+        
+        
+        output0.innerHTML=  `<div style="background-color: rgb(255, 255,255,0.2); padding: 1rem">Current price for this stock is: ${SP}</div>`;
 
     let CP = input1.value;
     let Qty = input2.value;
-    let SP = input3.value;
-
+    
     if( !isNaN(CP) && !isNaN(Qty) && !isNaN(SP)) {
         CP = Number(CP);
         Qty = Number(Qty);
         SP = Number(SP);
+        
         if(CP>0 && Qty>0 && SP>0){
             
             if (CP>SP){
                 const loss = ((CP-SP)*Qty).toFixed(2);
                 const lossPer = (((CP-SP)*100)/CP).toFixed(2);
-                output.innerHTML=  `<div style="background-color: rgb(255, 255,255,0.2); padding: 1rem">You lost ${lossPer}%. Your total loss is ₹${loss}</div>`;
+                output.innerHTML=  `<div style="background-color: rgb(255, 255,255,0.2); padding: 1rem">You lost ${lossPer}%. Your total loss is $${loss}</div>`;
 
             }
 
             else{
                 const profit = ((SP-CP)*Qty).toFixed(2)
                 const profitPer=(((SP-CP)*100)/CP).toFixed(2) ;
-                output.innerHTML=  `<div style="background-color: rgb(255, 255,255,0.2); padding: 1rem;" >You gained ${profitPer}%. Your total profit is ₹${profit}</div>`;
+                output.innerHTML=  `<div style="background-color: rgb(255, 255,255,0.2); padding: 1rem;" >You gained ${profitPer}%. Your total profit is $${profit}</div>`;
             }
         }
         else {
@@ -81,4 +82,12 @@ function checkHandler(e){
     else{
         output.innerHTML="Please enter values greater than 0 (only numbers are allowed in above fields"
     }
-}
+
+        })
+      
+        
+    
+    
+    .catch(error => console.log('error', error));
+    }
+  
